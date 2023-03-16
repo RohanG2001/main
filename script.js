0871b6c936e5a1e01bfb3f949713df4b5d1da51a
Rohan Ghorpade <64244858+RohanG2001@users.noreply.github.com>
2023-03-16 19:51:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+const options = {
+  bottom: '10px', // default: '32px'
+  right: '10px', // modified
+  left: 'auto', // modified
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff', // default: '#fff'
+  buttonColorDark: '#100f2c', // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '❤️', // default: ''
+  autoMatchOsTheme: true // default: true
+};
+
+  
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
+
+  
